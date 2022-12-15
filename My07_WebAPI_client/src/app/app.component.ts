4638d249af8a4cfa90ddb97cbcdc332709e13ab3
yand <yand@outlook.com>
2022-12-15 17:22:27 +0300

    YA

--- My07_WebAPI_client/src/app/app.component.ts
+++ My07_WebAPI_client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RateService } from 'src/services/rates';
+import { WeatherService } from 'src/services/weater';
 
 // Получение данных от Web API
 // Асинхронный запрос к веб-сервису сделан в файле services\rates.ts
 // В силу его асинхронности, страница уведомляется о получении результатов
 // с помощью механизма подписок Subject/Observables. 
-// (Подписка - здесь в конструкторе, публикация - файле services\rates.ts)
+// (Подписка - здесь в конструкторе, публикация - в файле services\rates.ts)
 
 @Component({
   selector: 'app-root',
@@ -15,12 +16,28 @@ import { RateService } from 'src/services/rates';
 export class AppComponent {
 
   usdRate = "loading...";
+  weatherData: any;
   
-  constructor(rateService: RateService) {
+  constructor(rateService: RateService, 
+              weatherService: WeatherService) {
     rateService.ready.subscribe( rates => {
       let result: any = rates;
       this.usdRate = result.Valute.USD.Value;
     } );
+
+    // Решение задачи о погоде:
+    this.weatherData = {"temperature": "...", "time": new Date()};
+
+    weatherService.ready.subscribe( data => {
+      console.log(data);
+      this.weatherData = data;
+    })
+
+    weatherService.getTemperature();
+
+    setInterval(() => {
+      weatherService.getTemperature();
+    }, 5000);
   }
 
 }
@@ -30,7 +47,5 @@ export class AppComponent {
 // которое один раз в 10 секунд запрашивает показания датчика и выводит на UI.
 // UI не должен "замерзать"! - не покатило из-за запрета CORs на этой службе.
 
-// Задача.Требуется сделать приложение, которое по таймеру обновляет показания темпертуры
+// Задача.Требуется сделать приложение, которое по таймеру обновляет показания температуры
 // в вашем городе. Должны показыватся ("тикать") время и температура.
-
-
